f539de43bc6a31ad1c8fe94b57d73ffc133eb9ea
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-09-22 12:23:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -339,4 +339,4 @@ def home():
     return render_template_string(HOME_HTML)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
