dfbac0a982def547979a8a5f07c3dba7c0846d87
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-11-24 22:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -244,4 +244,4 @@ def transcoded():
 
 # ---------------- Run on port 3000 ----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=8000)
